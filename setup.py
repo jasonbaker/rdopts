011edd49defef2127fdf24313016d50dbbebd58a
Jason Baker <amnorvend@gmail.com>
2010-04-29 13:17:08 -0500

    Adding description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+README_TEXT = open('./README.rst').read()
+
 setup(
     name='rdopts',
     version='0.1.0',
     py_modules=['rdopts'],
+    long_description=README_TEXT,
+    short_description='A custom setuptools command for reading from setup.cfg',
     entry_points= {
         'distutils.commands' : [
             'rdopts = rdopts:Rdopts']}
